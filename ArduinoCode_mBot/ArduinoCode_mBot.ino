d7f2bf2ee0397bc1744f276201d8ab688df4a083
sarbudragos <32175050+sarbudragos@users.noreply.github.com>
2017-10-15 11:43:07 +0300

    Update ArduinoCode_mBot.ino

--- ArduinoCode_mBot/ArduinoCode_mBot.ino
+++ ArduinoCode_mBot/ArduinoCode_mBot.ino
@@ -1,34 +1,63 @@
+ 
+/* define integer 'mesaj' */
 int mesaj;
 
+/* setup the connection */
 void setup() {
+  /* setting speed of connection */
   Serial.begin(9600);
+
+  /* */
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
 }
 
+/* executing commands */
 void loop() {
+
+  /* if the robot is connected */
   if (Serial.available() > 0)
-    {
+  {
+      /* read the ASCII code */
       mesaj = Serial.read();
 
+      /* switch the command by the ASCII code */
       switch(mesaj)
-        {
+      {
+          /* if ASCII code is 97 (a) */ 
           case 97:
+            /* switches off both leds*/
             digitalWrite(13, LOW);
             digitalWrite(12, LOW);
+
+            /* print the executed command */
             Serial.println  (" Received 'a' ");
             break;
+            
+          /* if ASCII code is 98 (b) */
           case 98:
+            /* lights up one led */
             digitalWrite(12, HIGH);
+
+            /* print the executed command */
             Serial.println(" Received 'b' ");
             break;
+            
+          /* if ASCII code is 99 (c) */
           case 99:
+            /*lights up the other led*/
             digitalWrite(13, HIGH);
+
+            /* print the executed command */
             Serial.println(" Received 'c' ");
             break;
+          /* if ASCII code is 100 (d) */
           case 100:
+            /* lights up both leds */
             digitalWrite(12, HIGH);
             digitalWrite(13, HIGH);
+
+            /* print the executed command */
             Serial.println(" Received 'd' ");
             break;
         }
